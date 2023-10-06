4ec724cb0d7f43e2381493aba436a7e4ac3db1b8
Rimma <nimdolphin94@gmail.com>
2023-10-06 12:47:45 +0300

    changed styles for StudentSearch component

--- src/components/StudentSearch/index.tsx
+++ src/components/StudentSearch/index.tsx
@@ -1,26 +1,39 @@
 import React, { FC, ChangeEvent } from "react";
+import { useTheme } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+
 import { StudentSearchProps } from "./types";
 
-import "./styles.scss";
+import { SearchContainer, SearchField } from "./styles";
 
 const StudentSearch: FC<StudentSearchProps> = ({
   searchName,
   setSearchText,
 }) => {
+  const theme = useTheme();
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
   };
 
   return (
-    <div className="search-container">
-      <span className="logo-icon"></span>
-      <input
+    <SearchContainer>
+      <SearchField
+        variant="outlined"
         type="text"
         placeholder="Поиск по имени"
         value={searchName}
         onChange={handleSearchChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        theme={theme}
       />
-    </div>
+    </SearchContainer>
   );
 };
 
---
+++ src/components/StudentSearch/styles.ts
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+import { Box, TextField } from "@mui/material";
+import { Theme } from "@mui/material/styles";
+
+export const SearchContainer = styled(Box)`
+  position: relative;
+  margin-bottom: 20px;
+`;
+
+export const SearchField = styled(TextField)(({ theme }: { theme: Theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  maxWidth: "100%",
+  border: "none",
+  width: "940px",
+  outline: "none",
+  fontSize: "20px",
+  "& .MuiOutlinedInput-notchedOutline": {
+    border: "none !important",
+  },
+  [theme.breakpoints.down("sm")]: {
+    marginLeft: "-50px",
+    height: "45px",
+    backgroundColor: "#fff",
+    maxWidth: "200px",
+    borderRadius: "3px",
+  },
+}));
